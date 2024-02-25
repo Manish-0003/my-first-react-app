95777431591a7e3c19b2ac0d09c77b138da42b4f
Manish-0003 <manish.sharma9314@gmail.com>
2024-02-25 10:51:04 +0530

    task 9

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,40 +1,88 @@
 import React, { useState } from "react";
+
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
-    const[enteredTitle, setEnteredTitle] = useState ('');
-    const[EnteredAmount,setEnteredAmount] = useState('');
-    const[EnteredDate, setEnteredDate] = useState('');
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate:''
 
-    const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-        
+  // });
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // })
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // })
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // })
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
     };
 
-    const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-    }
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
 
-    const dateChangeHandler = event => {
-        setEnteredDate(event.target.value)
-    }
-    
   return (
-    <form>
-      <div className="new-expense__controls">;</div>
-      <div className="new-expense--control"></div>
-      <label>Title</label>
-      <input type="text"  onChange={titleChangeHandler} />
-      <div className="new-expense--control"></div>
-      <label>Amount</label>
-      <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
-      <div className="new-expense--control"></div>
-      <label>Date</label>
-      <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
-
+    <form oonSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense--control">
+          <label>Title</label>
+          <input type="text"  value={enteredTitle}
+            onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense--control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense--control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
       <div className="new-expense__actions">
         <button type="submit">Add Expense </button>
-        
       </div>
     </form>
   );
